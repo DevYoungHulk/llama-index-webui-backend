5809e1bf9552f5d59611ee76ae00a547263e16d4
yanghao18 <yanghao18@lenovo.com>
2024-04-03 14:44:20 +0800

    add none development config

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ from app import create_app
 import traceback
 from flask import jsonify
 from flask_cors import CORS
-
+import os
 import logging
 # 默认为开发环境，按需求修改
-config_name = 'development'
+config_name = os.environ.get('ENV_NAME', 'development')
 
 app = create_app(config_name)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 logger = logging.getLogger('root')
+
 
 @app.errorhandler(Exception)
 def handle_exception(e):
@@ -24,4 +25,8 @@ def handle_exception(e):
 
 
 if __name__ == '__main__':
-    app.run()
+    if config_name == 'development':
+        app.run(host='0.0.0.0', port=5000, debug=True)
+    else:
+        from waitress import serve
+        serve(app, host="0.0.0.0", port=5000)
